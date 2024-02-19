d4c71d10f8a66181db4118634bf6338a707ba95e
kalai joys <kjayaku1@uncc.edu>
2024-02-18 21:32:13 -0500

    brought personal budget code to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
 
+import Menu from './Menu/Menu';
+import AboutPage from './AboutPage/AboutPage';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import LoginPage from './LoginPage/LoginPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < Router>
+   <Menu/>
+      <Hero/>
+      <div div className="mainContainer">
+      <switch>
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+
+      </switch>
+      </div>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    
+      <div>
+      <div className="bottom"> 
+    <div className="center">
+        All rights reserved &copy; Kalai Arasi
+    </div>
+</div>
+      </div>
+    
+  );
+}
+
+export default Footer;
